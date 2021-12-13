a19181ffd3b47f938116045c42a080581a91d0e6
Laucherooo <cc243@njit.edu>
2021-12-13 03:23:52 -0500

    change something in homepage and the urls

--- app/controllers/calculator_controller.py
+++ app/controllers/calculator_controller.py
@@ -14,8 +14,18 @@ class CalculatorController(ControllerBase):
             value1 = request.form['value1']
             value2 = request.form['value2']
             operation = request.form['operation']
+            value1 = float(value1)
+            value2 = float(value2)
             # make the tuple
-            my_tuple = (value1, value2)
+            if (operation == 'addition'):
+                result = value1 + value2
+            elif (operation == 'substraction'):
+                result = value1 - value2
+            elif (operation == 'multiplication'):
+                result = value1 * value2
+            elif (operation == 'division'):
+                result = value1 / value2
+            """my_tuple = (value1, value2)
             # this will call the correct operation
             getattr(Calculator, operation)(my_tuple)
             result = str(Calculator.get_last_result_value())
@@ -24,8 +34,10 @@ class CalculatorController(ControllerBase):
                 'value2': [value2],
                 'operation': [operation]
             }
-            Calculator.writeHistoryToCSV()
-            return render_template('result.html', data = Calculator.getHistory(), value1=value1, value2=value2, operation=operation, result=result)
+            
+            Calculator.writeHistoryToCSV()"""
+            return render_template('result.html', value1=value1, value2=value2, operation=operation, result=result)
+            #return render_template('result.html', data = Calculator.getHistory(), value1=value1, value2=value2, operation=operation, result=result)
         return render_template('calculator.html', error=error)
     @staticmethod
     def get():
